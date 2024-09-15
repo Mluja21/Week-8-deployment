6b416a2b0eedfb6dc00ff768f880e98c089c9aac
Mluja21 <simelanem437@gmail.com>
2024-09-15 20:07:21 +0200

    initial commit

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql2");
+
+require("dotenv").config();
+
+// Create connection
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// Establish the connection to the database
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+module.exports = db;
